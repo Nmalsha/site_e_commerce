c28d85dcf0fcd546f1bd1339916de9cc92056459
MalPro <malshis@yahoo.com>
2024-07-19 15:04:06 +0200

    adding contact form

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -2,9 +2,86 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="container mt-5">
-      <h2>Contact</h2>
-      <p>Contactez-nous pour toute question.</p>
+    <div
+      className="container mt-5"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "20px",
+          width: "60%",
+          backgroundColor: "#5b4b61",
+        }}
+      >
+        <form
+          style={{
+            display: "flex",
+            width: "80%",
+            justifyContent: "center",
+            flexDirection: "column",
+            marginTop: "20px",
+            borderRadius: "20px",
+            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <h2 style={{ marginBottom: "20px" }}>Contactez nous</h2>
+          <div style={{ marginTop: "20px" }}>
+            <div className="mb-3">
+              <label htmlFor="nom" className="form-label">
+                Nom
+              </label>
+              <input type="text" className="form-control" id="nom" required />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="prenom" className="form-label">
+                Prénom
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="prenom"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="tp" className="form-label">
+                TP
+              </label>
+              <input type="number" className="form-control" id="tp" required />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="message" className="form-label">
+                Message
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="message"
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-secondary">
+              contect
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
